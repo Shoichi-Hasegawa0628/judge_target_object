0eee3568701797e1729ece4f2b24ed79505e9073
HasegawaShoichi <0ve38x163040p4z@ezweb.ne.jp>
2021-08-16 16:32:36 +0900

    WIP: Created judge_mlda

--- src/judge_target_object_send_img.py
+++ src/judge_target_object_send_img.py
@@ -17,16 +17,16 @@ class SendObjectImage():
 
     def __init__(self):
         # YOLOv3
-        self.img_pub = rospy.Publisher('/object_image', Image, queue_size=1)
-        rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.bounding_callback, queue_size=1)
+        #self.img_pub = rospy.Publisher('/object_image', Image, queue_size=1)
+        #rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.bounding_callback, queue_size=1)
 
         # MLDA
         #self.cut_img_pub = rospy.Publisher('/cut_object_image', Image, queue_size=10)
         
         self.cv_bridge = CvBridge()
         self.detect_objects_info = []
-        self.sending_image_judge_yolov3()
-        #self.sending_image_judge_mlda()
+        #self.sending_image_judge_yolov3()
+        self.sending_image_judge_mlda()
 
 
     def sending_image_judge_yolov3(self):
@@ -46,7 +46,21 @@ class SendObjectImage():
                 print(response)
                 time.sleep(1.0)
                 count += 1
+    
+
+    def sending_image_judge_mlda(self):
+        rospy.wait_for_service('judge_mlda')
+        img = cv2.imread('../data/trimming/0/trimming_img_0_re.jpg')
+        img = self.cv_bridge.cv2_to_imgmsg(img, encoding="bgr8")
+        send_img = rospy.ServiceProxy('judge_mlda', SendImageMLDA)
+        yolov3_image = img
+        status = "estimate"
+        count = 0
+
+        response = send_img(yolov3_image, status, count)
+        print(response)
         
+
     """
     def sending_image_judge_mlda(self):
         count = 0
@@ -66,8 +80,6 @@ class SendObjectImage():
             while count != int(files_list[k]):
                     img = cv2.imread('../data/trimming/{}/trimming_img_{}.jpg'.format(k, count))
                     img = self.cv_bridge.cv2_to_imgmsg(img, encoding="bgr8")
-                    while len(self.detect_objects_info) == 0:
-                        self.img_pub.publish(img)
                     send_img = rospy.ServiceProxy('judge_mlda', SendImageMLDA)
                     rgb_image = img
                     response = send_img(rgb_image, count)
---
+++ src/trial.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# MLDAで使用する画像特徴量 (BoF)を計算するコード
+
+import cv2
+origin = cv2.imread("../data/observation/object_image_0.jpg")
+height_o = origin.shape[0]
+width_o = origin.shape[1]
+
+img = cv2.imread("../data/trimming/0/trimming_img_0.jpg")
+height = img.shape[0]
+width = img.shape[1]
+
+#img3 = cv2.resize(img , (int(width*1.5), int(height*1.5)))
+img3 = cv2.resize(img , (int(width_o), int(height_o )))
+cv2.imwrite('../data/trimming/0/trimming_img_0_re.jpg' , img3)
