e72973e877d806ab89f65e8d71429a9c3198efa9
Shoichi-Hasegawa0628 <hasegawa.shoichi@em.ci.ritsumei.ac.jp>
2021-10-22 12:45:08 +0900

    WIP: Fix code, only check program moving

---
+++ bash/reset-judge-output.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -r ../data/*
+
+mkdir -p ../data/observation/
+mkdir -p ../data/resize/
+mkdir -p ../data/trimming/
+mkdir -p ../data/yolov3/
--- src/judge_target_object_mlda_main.py
+++ src/judge_target_object_mlda_main.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# MLDAのコードを統括するコード
+# MLDAに処理対象の画像を送り、推定結果を返すプログラム
 
+# Standard Library
+import sys
+
+# Third Party
 import rospy
+import roslib.packages
+
+# Self-made Modules
 #from mlda_ros.srv import SendImageMLDA
 #from mlda_ros.srv import SendImageMLDAResponse
-import judge_target_object_mlda_image
-import judge_target_object_mlda_learn
-import judge_target_object_mlda_word
+sys.path.append(str(roslib.packages.get_pkg_dir("mlda")) + "/scripts")
+import extract_img_bof
+import execute_node
+import extract_word_bow
 
 
 class MLDAMain():
     
     def __init__(self):
-        self.mlda_image = judge_target_object_mlda_image.GetImageFeature()
-        self.mlda_learn = judge_target_object_mlda_learn.MLDA()
-        self.mlda_word = judge_target_object_mlda_word.GetWordFeature()
+        self.mlda_image = extract_img_bof.ExtractImgBof()
+        self.mlda_learn = execute_node.MLDA()
+        self.mlda_word = extract_word_bow.ExtractWordBow()
         #rospy.Service('judge_mlda', SendImageMLDA, self.judge_target_object_mlda)
     
 
     def judge_target_object_mlda(self, yolov3_image, status, observed_img_idx, count):
-        success = 0
         if status == "estimate":
-            #print("Start")                                                        # 物体の画像から物体の単語を予測
-            result = self.mlda_image.image_server(yolov3_image, status, observed_img_idx, count)
+            result = self.mlda_image.image_server(status, count, observed_img_idx, yolov3_image)
             if result == 0:
                 success = False
                 object_word = None
@@ -31,9 +37,7 @@ class MLDAMain():
                 return success, object_word, object_prob
             self.mlda_learn.mlda_server(status, observed_img_idx, count)
             result = self.mlda_word.word_server(yolov3_image, status, observed_img_idx, count)
- 
-        #else:                                                                               # データセットからパラメータを学習
-        #    pass
+
         object_word = []
         #object_word_list = []
         object_prob = []
